6a65f9aa1c0ceb4d7bd6e8d5027ac7e1c6bc2e61
Shiva Sunchu <chhotahathi06@gmail.com>
2023-06-24 11:30:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading as="h1">Home page</Heading>
+    </div>
+  );
+}
+
+export default App;
